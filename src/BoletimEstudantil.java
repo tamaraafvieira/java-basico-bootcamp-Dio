9e2310850ed6aff23e0aa8864b10735250098771
= <76081373+tamaraafvieira@users.noreply.github.com>
2022-10-25 18:51:25 -0300

    Boletim estudantil

---
+++ src/BoletimEstudantil.java
@@ -0,0 +1,14 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+
+        if(mediaFinal < 6)
+            System.out.println("Reprovado");
+        else if (mediaFinal == 6)
+            System.out.println("Prova de recuperacao");
+        else
+            System.out.println("Aprovado");
+
+    }
+
+}
